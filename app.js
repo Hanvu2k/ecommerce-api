d23e6eed2e91ed172707e05c6c4964043d12601b
Hanvu2k <114717404+Hanvu2k@users.noreply.github.com>
2023-09-06 15:38:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const { connect } = require("./config/db");
 
 const methods = ["GET", "POST", "PATCH", "DELETE"];
 const corsOptions = {
-  origin: "*",
+  origin: ["https://ecommerce-server-hqbv.onrender.com"],
   methods: methods,
   credentials: true,
 };
